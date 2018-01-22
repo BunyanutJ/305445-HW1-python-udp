97e3f90e7d3c3cd9e438f8f7215e07a54882ed12
บุณยานุช จิระโพธิ์ <bunyanutj58@nu.ac.th>
2018-01-22 21:54:39 +0700

    Homework1-UDP

--- client.py
+++ client.py
@@ -1,13 +1,17 @@
 import socket
 
 UDP_IP = "127.0.0.1"
-UDP_PORT = 5005
-MESSAGE = "Hello, World!"
+UDP_PORT = 5020
+number = "5"
 
 print "UDP target IP:", UDP_IP
 print "UDP target port:", UDP_PORT
-print "message:", MESSAGE
+print "message:", number
 
 sock = socket.socket(socket.AF_INET, # Internet
                      socket.SOCK_DGRAM) # UDP
-sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
+sock.sendto(number, (UDP_IP, UDP_PORT))
+
+while True:
+    data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
+    print "\n\nResult of factorial is", data
--- server.py
+++ server.py
@@ -1,9 +1,18 @@
 import socket
 UDP_IP = "127.0.0.1"
-UDP_PORT = 5005
+UDP_PORT = 5020
+summation = 1
+
 sock = socket.socket(socket.AF_INET, # Internet
                      socket.SOCK_DGRAM) # UDP
 sock.bind((UDP_IP, UDP_PORT))
 while True:
     data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
-    print "received message:", data
+    n = int(data)
+    print "Number:", n
+    while n>0:
+        summation = summation*n
+        n=n-1
+    print "Result :", summation
+    s = str(summation)
+    sock.sendto(s, addr)
